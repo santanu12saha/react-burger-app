c73243043ad6f638256ab7066cee08e84bc6c686
santanu12saha <saha.santanu0217@gmail.com>
2020-01-05 12:11:21 +0530

    Change functional stateless component to statefull component and added life cycle hook method for performance improvement

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+    // This could be a functional component, doesn't have to be a class (statefull component) 
+    UNSAFE_componentWillUpdate(){
+        console.log('[OrderSummary] will update');
+    }
+
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igkey => {
         return (
             <li key={igkey}>
-                <span style={{textTransform: 'capitalize'}}>{igkey}</span>: {props.ingredients[igkey]}
+                <span style={{textTransform: 'capitalize'}}>{igkey}</span>: {this.props.ingredients[igkey]}
             </li> );
         });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
-};
 
-export default orderSummary;
+        return(
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+}
+
+export default OrderSummary;
