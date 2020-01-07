37099e27a7fc4961be42fe7355ce7b244cb4af54
santanu12saha <saha.santanu0217@gmail.com>
2020-01-08 00:59:09 +0530

    Checked for clearing the interceptors for a particular component will unmout (by dummy hidding of burger builder)

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+
+  state = {
+    show: true
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({show: false});
+    }, 5000)
+  }
+
   render(){
     return (
       <div className={classes.Font}>
         <Layout>
-          <BurgerBuilder/>
+          {this.state.show ? <BurgerBuilder/> : null}
         </Layout>
       </div>
     );
