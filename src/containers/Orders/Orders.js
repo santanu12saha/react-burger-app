501dfab578d0bfdde53e5135bae551b2514a4928
santanu12saha <saha.santanu0217@gmail.com>
2020-01-15 13:08:00 +0530

    Fetch order data from backend and manipulate single order data to put in UI.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,15 +1,56 @@
 import React, { Component } from "react";
+
 import Order from '../../components/Order/Order';
+import Spinner from '../../components/UI/Spinner/Spinner';
+
+import axios from '../../axios-order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component{
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchOrders = [];
+                for(let key in res.data){
+                    fetchOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({loading: false, orders: fetchOrders});
+            })
+            .catch(err => {
+                this.setState({loading: false});
+            })
+    }
+
     render(){
+
+        let orders = null;
+
+        if(this.state.loading){
+            orders = <Spinner />;
+        }else{
+            orders = this.state.orders.map(order => (
+                <Order 
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={order.price}/>
+            ));
+        }
+
         return (
             <div>
-                <Order/>
-                <Order/>
+               {orders}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
