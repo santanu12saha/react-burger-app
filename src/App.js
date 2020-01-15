de091c866acb5eeb1f777858f485c04597ed9774
santanu12saha <saha.santanu0217@gmail.com>
2020-01-15 11:42:07 +0530

    Added Order Component and provide NavLink for the same

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import classes from './App.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render(){
@@ -12,8 +13,9 @@ class App extends Component {
       <div className={classes.Font}>
         <Layout>
           <Switch>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/" exact component={BurgerBuilder}/>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
       </div>
