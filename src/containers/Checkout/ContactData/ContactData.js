814c692d4f4f37ece42c7d94d06c037c3cc6c52e
santanu12saha <saha.santanu0217@gmail.com>
2020-01-18 11:00:38 +0530

    Adding dynamic input element and select dropdown.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,32 +8,68 @@ import axios from '../../../axios-order';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                   options: [
+                       {value: 'fastest', displayValue: 'Fastest'},
+                       {value: 'cheapest', displayValue: 'Cheapest'}
+                   ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
-
+ 
     orderHandler = (event) => {
         event.preventDefault();
         
         this.setState({ loading: true });
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price,
-            customer: {
-                name: 'Santanu Saha',
-                address: {
-                    street: 'TestStreet 1',
-                    zipCode: '12345',
-                    country: 'Germany'
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price,    
         }
         
         axios.post('/orders.json', order)
@@ -47,12 +83,23 @@ class ContactData extends Component {
     }
 
     render(){
+        const formElementArray = [];
+        for(let key in this.state.orderForm){
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key] 
+            })
+        }
+
         let form = (
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
-                <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+                {formElementArray.map(formElement => (
+                   <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}/> 
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
