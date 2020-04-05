8de29c52417483f90ac72fa15daeef927c167601
santanu12saha <saha.santanu0217@gmail.com>
2020-04-05 20:59:12 +0530

    Add Redux saga for logout actions.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,11 +23,8 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('userId');
     return {
-        type: actionTypes.AUTH_LOGOUT
+        type: actionTypes.AUTH_INITIATE_LOGOUT
     }
 };
 
