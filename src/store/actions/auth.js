7188635f433f1807e4a151f02632d670ce0fe9ac
santanu12saha <saha.santanu0217@gmail.com>
2020-04-11 13:01:13 +0530

    Implmenting auth check redux saga and replace redux - thunk implementation to saga.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -57,19 +57,7 @@ export const setAuthRedirectPath = (path) => {
 };
 
 export const authCheckState = () => {
-    return dispatch => {
-        const token = localStorage.getItem('token');
-        if(!token){
-            dispatch(logout());
-        }else{
-            const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if(expirationDate >= new Date()) {
-                const userId = localStorage.getItem('userId');
-                dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000 ));
-            }else{
-                dispatch(logout());
-            } 
-        }
+    return {
+        type: actionTypes.AUTH_CHECK_STATE
     };
 };
